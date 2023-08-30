f75f0a03681ce819a9e7fb8c8398f5e037fed4d4
umacair <91578981+umacair@users.noreply.github.com>
2023-08-30 22:20:43 +0900

    test
    ST

--- webserver.js
+++ webserver.js
@@ -170,7 +170,7 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
 
   socket.on('light', function(data) { //get light switch status from client
     lightvalue = data;
-    if (lightvalue != !LED.readSync()) { //only change LED if status has changed
+    if (lightvalue == LED.readSync()) { //only change LED if status has changed
         LED.writeSync(!lightvalue); //turn LED on or of
     }
   });
